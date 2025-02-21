016b5fd9d2aed04d3a94867c8baac952666a0f27
Yoshikage Kira <8bit_yoshikage_kira@proton.me>
2025-02-20 23:13:49 -0500

    Remove service enable since it was not needed

--- add_service_files.sh
+++ add_service_files.sh
@@ -4,6 +4,4 @@ mkdir -p ~/.config/systemd/user
 ln -fsv "$(realpath systemd_service_files/ddns-updater.service)" ~/.config/systemd/user/ddns-updater.service
 ln -fsv  "$(realpath systemd_service_files/ddns-updater.timer)" ~/.config/systemd/user/ddns-updater.timer
 
-systemctl --user enable ddns-updater.service
-systemctl --user enable ddns-updater.timer
-systemctl --user start ddns-updater.timer
+systemctl --user enable --now ddns-updater.timer
