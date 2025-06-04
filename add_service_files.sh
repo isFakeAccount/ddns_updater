490672e92e356c4dc745b181c6f5cd5d783c963a
Yoshikage Kira <8bit_yoshikage_kira@proton.me>
2025-06-04 01:24:16 -0400

    Changed the symlinked bin name
    - Changed the symlinked bin name
    - Update the add service_files to symlink the binary

--- add_service_files.sh
+++ add_service_files.sh
@@ -2,5 +2,6 @@
 
 ln -fsv "$(realpath systemd_service_files/ddns-updater.service)" /etc/systemd/system/ddns-updater.service
 ln -fsv  "$(realpath systemd_service_files/ddns-updater.timer)" /etc/systemd/system/ddns-updater.timer
+ln -fsv "$(realpath main.py)" /usr/local/bin/ddns_updater
 
 systemctl enable --now ddns-updater.timer
